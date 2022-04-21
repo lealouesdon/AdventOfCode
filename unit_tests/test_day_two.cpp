03c48f1ab9eb1ef977733fb709ab9c88bd5911ac
Lea Louesdon <lea.louesdon@resmed.com>
2022-04-21 15:51:42 +0100

    Revert "empty for pull request"
    This reverts commit ec21cd4a6742c17f3e5698cfa11bae17452898b2.

--- unit_tests/test_day_two.cpp
+++ unit_tests/test_day_two.cpp
@@ -9,5 +9,19 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
+    ifstream testFile("../source_code/inputs/day_two.txt");    
+    string line;
 
+    Length completeSize;
+    while(getline(testFile, line)){
+        Present p;
+        replace(line.begin(), line.end(), 'x', ' ');
+        stringstream ss(line);
+        ss >> p.height >> p.length >> p.width;
+        std::cout << p.wrapping_paper_calculator() << std::endl;
+        completeSize += p.wrapping_paper_calculator();
+        std::cout << "New size " << completeSize << std::endl;
+    }
+    
+    std::cout << "Needed size : "<< completeSize << std::endl;
 }
